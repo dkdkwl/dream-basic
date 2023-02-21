949984f0a936aec29697b39af4ae4fe2f10987e1
dkdkwl <dkdkwl@naver.com>
2023-02-21 13:36:25 +0900

    fetch를 사용해서 데이터 가져오기

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,17 +4,18 @@ export default function Products() {
   const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
 
-  useEffect(() => {
+  useEffect(()=>{
     fetch('data/products.json')
-      .then((res) => res.json())
-      .then((data) => {
-        console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
-        setProducts(data);
+      .then(res => res.json())
+      .then(data => {
+        console.log("뜨끈한 데이터를 네트워크에서 받아옴")
+        setProducts(data)
       });
-    return () => {
-      console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
-    };
-  }, []);
+      return ()=>{
+        console.log('깨끗하게 청소하는 일들을 합니다')
+      }
+  },[])
+
 
   return (
     <>
