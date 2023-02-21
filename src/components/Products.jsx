3a34a1cb6ea2aea8de5a88b0494d419c3d2333cc
dkdkwl <dkdkwl@naver.com>
2023-02-21 13:45:19 +0900

    useEffect를 활용해서 json 데이터 보여주고 안보여주고 처리하기

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Products() {
-  const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
-
+  const [checked,setChecked] = useState(false);
+  const handleChange = ()=> setChecked((prev)=> !prev);
   useEffect(()=>{
-    fetch('data/products.json')
+    fetch(`data/${checked ? 'sale_' : ''}products.json`)
       .then(res => res.json())
       .then(data => {
         console.log("뜨끈한 데이터를 네트워크에서 받아옴")
@@ -14,22 +14,23 @@ export default function Products() {
       return ()=>{
         console.log('깨끗하게 청소하는 일들을 합니다')
       }
-  },[])
+  },[checked])
 
 
   return (
     <>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <article>
-              <h3>{product.name}</h3>
-              <p>{product.price}</p>
-            </article>
-          </li>
-        ))}
-      </ul>
-      <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
+      <input type="checkbox"value={checked}onChange={handleChange}/>
+        <label htmlFor='checkbox'>Show Only 핫 Sale</label>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              <article>
+                <h3>{product.name}</h3>
+                <p>{product.price}</p>
+              </article>
+            </li>
+          ))}
+        </ul>
     </>
   );
 }
